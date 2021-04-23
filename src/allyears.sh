50cf515bb6f54f52ed42e76acf56cf86b783a76b
Kim Fisher <kfisher@wcs.org>
2021-04-23 10:47:28 -0400

    WIP running Leuser on old SCL polys

---
+++ src/allyears.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+python scl_stats.py -d 2014-01-01
+python scl_stats.py -d 2013-01-01
+python scl_stats.py -d 2012-01-01
+python scl_stats.py -d 2011-01-01
+python scl_stats.py -d 2010-01-01
+python scl_stats.py -d 2009-01-01
+python scl_stats.py -d 2008-01-01
+python scl_stats.py -d 2007-01-01
+python scl_stats.py -d 2006-01-01
+python scl_stats.py -d 2005-01-01
+python scl_stats.py -d 2004-01-01
+python scl_stats.py -d 2003-01-01
--- src/scl_stats.py
+++ src/scl_stats.py
@@ -4,6 +4,11 @@ from datetime import datetime, timezone
 from task_base import SCLTask
 
 
+# USDOS/LSIB/2017 apparently dropped iso support
+USDOS_NAME_FIELD = "COUNTRY_NA"
+ISO_FIELD = "iso_alpha2"
+
+
 class SCLStats(SCLTask):
     ee_rootdir = "projects/SCL/v1"
     inputs = {
@@ -12,36 +17,305 @@ class SCLStats(SCLTask):
             "ee_path": f"scl_path_{SCLTask.SPECIES}",
             "maxage": 1 / 365,  # years
         },
-        "scl_restoration": {
-            "ee_type": SCLTask.FEATURECOLLECTION,
-            "ee_path": f"scl_path_{SCLTask.RESTORATION}",
-            "maxage": 1 / 365,
-        },
+        # "scl_restoration": {
+        #     "ee_type": SCLTask.FEATURECOLLECTION,
+        #     "ee_path": f"scl_path_{SCLTask.RESTORATION}",
+        #     "maxage": 1 / 365,
+        # },
         "scl_survey": {
             "ee_type": SCLTask.FEATURECOLLECTION,
             "ee_path": f"scl_path_{SCLTask.SURVEY}",
             "maxage": 1 / 365,
         },
-        "scl_fragment": {
-            "ee_type": SCLTask.FEATURECOLLECTION,
-            "ee_path": f"scl_path_{SCLTask.FRAGMENT}",
-            "maxage": 1 / 365,
-        },
+        # "scl_fragment": {
+        #     "ee_type": SCLTask.FEATURECOLLECTION,
+        #     "ee_path": f"scl_path_{SCLTask.FRAGMENT}",
+        #     "maxage": 1 / 365,
+        # },
         "countries": {
             "ee_type": SCLTask.FEATURECOLLECTION,
-            "ee_path": "USDOS/LSIB/2013",
-            "maxage": 10,
+            "ee_path": "USDOS/LSIB/2017",  # 2013 also exists
+            "static": True,
+        },
+        "leuser": {
+            "ee_type": SCLTask.FEATURECOLLECTION,
+            "ee_path": "projects/SCL/v1/Panthera_tigris/geographies/Sumatra/leuser",
+            "static": True,
         },
         "ecoregions": {
             "ee_type": SCLTask.FEATURECOLLECTION,
             "ee_path": "RESOLVE/ECOREGIONS/2017",
-            "maxage": 5,
+            "static": True,
         },
         "pas": {
             "ee_type": SCLTask.FEATURECOLLECTION,
             "ee_path": "WCMC/WDPA/current/polygons",
             "maxage": 1,
+            # TODO:
+            # WCMC updates the WDPA on a monthly basis. The most recent version is always available as
+            # WCMC/WDPA/current/polygons and WCMC/WDPA/current/points. Historical versions, starting with July 2017,
+            # are available in the format WCMC/WDPA/YYYYMM/polygons and WCMC/WDPA/YYYYMM/points.
         },
+    }
+
+    # temporary legacy for Leuser
+    def _scl_path(self, scltype):
+        if scltype is None or scltype not in self.LANDSCAPE_TYPES:
+            raise TypeError("Missing or incorrect scltype for setting scl path")
+        return f"projects/SCL/v1/Panthera_tigris/geographies/Sumatra/scl_poly/{self.taskdate}/scl_{scltype}"
+
+    # matches (ISO 3166-1) django_countries used in scl-api
+    ISO_COUNTRY_DICT = {
+        "Afghanistan": "AF",
+        "Åland Islands": "AX",
+        "Albania": "AL",
+        "Algeria": "DZ",
+        "American Samoa": "AS",
+        "Andorra": "AD",
+        "Angola": "AO",
+        "Anguilla": "AI",
+        "Antarctica": "AQ",
+        "Antigua and Barbuda": "AG",
+        "Argentina": "AR",
+        "Armenia": "AM",
+        "Aruba": "AW",
+        "Australia": "AU",
+        "Austria": "AT",
+        "Azerbaijan": "AZ",
+        "Bahamas": "BS",
+        "Bahrain": "BH",
+        "Bangladesh": "BD",
+        "Barbados": "BB",
+        "Belarus": "BY",
+        "Belgium": "BE",
+        "Belize": "BZ",
+        "Benin": "BJ",
+        "Bermuda": "BM",
+        "Bhutan": "BT",
+        "Bolivia": "BO",
+        "Bonaire, Sint Eustatius and Saba": "BQ",
+        "Bosnia and Herzegovina": "BA",
+        "Botswana": "BW",
+        "Bouvet Island": "BV",
+        "Brazil": "BR",
+        "British Indian Ocean Territory": "IO",
+        "Brunei": "BN",
+        "Bulgaria": "BG",
+        "Burkina Faso": "BF",
+        "Burundi": "BI",
+        "Cabo Verde": "CV",
+        "Cambodia": "KH",
+        "Cameroon": "CM",
+        "Canada": "CA",
+        "Cayman Islands": "KY",
+        "Central African Republic": "CF",
+        "Chad": "TD",
+        "Chile": "CL",
+        "China": "CN",
+        "Christmas Island": "CX",
+        "Cocos (Keeling) Islands": "CC",
+        "Colombia": "CO",
+        "Comoros": "KM",
+        "Congo": "CG",
+        "Congo (the Democratic Republic of the)": "CD",
+        "Cook Islands": "CK",
+        "Costa Rica": "CR",
+        "Côte d'Ivoire": "CI",
+        "Croatia": "HR",
+        "Cuba": "CU",
+        "Curaçao": "CW",
+        "Cyprus": "CY",
+        "Czechia": "CZ",
+        "Denmark": "DK",
+        "Djibouti": "DJ",
+        "Dominica": "DM",
+        "Dominican Republic": "DO",
+        "Ecuador": "EC",
+        "Egypt": "EG",
+        "El Salvador": "SV",
+        "Equatorial Guinea": "GQ",
+        "Eritrea": "ER",
+        "Estonia": "EE",
+        "Eswatini": "SZ",
+        "Ethiopia": "ET",
+        "Falkland Islands (Malvinas)": "FK",
+        "Faroe Islands": "FO",
+        "Fiji": "FJ",
+        "Finland": "FI",
+        "France": "FR",
+        "French Guiana": "GF",
+        "French Polynesia": "PF",
+        "French Southern Territories": "TF",
+        "Gabon": "GA",
+        "Gambia": "GM",
+        "Georgia": "GE",
+        "Germany": "DE",
+        "Ghana": "GH",
+        "Gibraltar": "GI",
+        "Greece": "GR",
+        "Greenland": "GL",
+        "Grenada": "GD",
+        "Guadeloupe": "GP",
+        "Guam": "GU",
+        "Guatemala": "GT",
+        "Guernsey": "GG",
+        "Guinea": "GN",
+        "Guinea-Bissau": "GW",
+        "Guyana": "GY",
+        "Haiti": "HT",
+        "Heard Island and McDonald Islands": "HM",
+        "Holy See": "VA",
+        "Honduras": "HN",
+        "Hong Kong": "HK",
+        "Hungary": "HU",
+        "Iceland": "IS",
+        "India": "IN",
+        "Indonesia": "ID",
+        "Iran": "IR",
+        "Iraq": "IQ",
+        "Ireland": "IE",
+        "Isle of Man": "IM",
+        "Israel": "IL",
+        "Italy": "IT",
+        "Jamaica": "JM",
+        "Japan": "JP",
+        "Jersey": "JE",
+        "Jordan": "JO",
+        "Kazakhstan": "KZ",
+        "Kenya": "KE",
+        "Kiribati": "KI",
+        "Kuwait": "KW",
+        "Kyrgyzstan": "KG",
+        "Laos": "LA",
+        "Latvia": "LV",
+        "Lebanon": "LB",
+        "Lesotho": "LS",
+        "Leuser": "LE",
+        "Liberia": "LR",
+        "Libya": "LY",
+        "Liechtenstein": "LI",
+        "Lithuania": "LT",
+        "Luxembourg": "LU",
+        "Macao": "MO",
+        "Madagascar": "MG",
+        "Malawi": "MW",
+        "Malaysia": "MY",
+        "Maldives": "MV",
+        "Mali": "ML",
+        "Malta": "MT",
+        "Marshall Islands": "MH",
+        "Martinique": "MQ",
+        "Mauritania": "MR",
+        "Mauritius": "MU",
+        "Mayotte": "YT",
+        "Mexico": "MX",
+        "Micronesia (Federated States of)": "FM",
+        "Moldova": "MD",
+        "Monaco": "MC",
+        "Mongolia": "MN",
+        "Montenegro": "ME",
+        "Montserrat": "MS",
+        "Morocco": "MA",
+        "Mozambique": "MZ",
+        "Myanmar": "MM",
+        "Namibia": "NA",
+        "Nauru": "NR",
+        "Nepal": "NP",
+        "Netherlands": "NL",
+        "New Caledonia": "NC",
+        "New Zealand": "NZ",
+        "Nicaragua": "NI",
+        "Niger": "NE",
+        "Nigeria": "NG",
+        "Niue": "NU",
+        "Norfolk Island": "NF",
+        "North Korea": "KP",
+        "North Macedonia": "MK",
+        "Northern Mariana Islands": "MP",
+        "Norway": "NO",
+        "Oman": "OM",
+        "Pakistan": "PK",
+        "Palau": "PW",
+        "Palestine, State of": "PS",
+        "Panama": "PA",
+        "Papua New Guinea": "PG",
+        "Paraguay": "PY",
+        "Peru": "PE",
+        "Philippines": "PH",
+        "Pitcairn": "PN",
+        "Poland": "PL",
+        "Portugal": "PT",
+        "Puerto Rico": "PR",
+        "Qatar": "QA",
+        "Réunion": "RE",
+        "Romania": "RO",
+        "Russia": "RU",
+        "Rwanda": "RW",
+        "Saint Barthélemy": "BL",
+        "Saint Helena, Ascension and Tristan da Cunha": "SH",
+        "Saint Kitts and Nevis": "KN",
+        "Saint Lucia": "LC",
+        "Saint Martin (French part)": "MF",
+        "Saint Pierre and Miquelon": "PM",
+        "Saint Vincent and the Grenadines": "VC",
+        "Samoa": "WS",
+        "San Marino": "SM",
+        "Sao Tome and Principe": "ST",
+        "Saudi Arabia": "SA",
+        "Senegal": "SN",
+        "Serbia": "RS",
+        "Seychelles": "SC",
+        "Sierra Leone": "SL",
+        "Singapore": "SG",
+        "Sint Maarten (Dutch part)": "SX",
+        "Slovakia": "SK",
+        "Slovenia": "SI",
+        "Solomon Islands": "SB",
+        "Somalia": "SO",
+        "South Africa": "ZA",
+        "South Georgia and the South Sandwich Islands": "GS",
+        "South Korea": "KR",
+        "South Sudan": "SS",
+        "Spain": "ES",
+        "Sri Lanka": "LK",
+        "Sudan": "SD",
+        "Suriname": "SR",
+        "Svalbard and Jan Mayen": "SJ",
+        "Sweden": "SE",
+        "Switzerland": "CH",
+        "Syria": "SY",
+        "Taiwan": "TW",
+        "Tajikistan": "TJ",
+        "Tanzania": "TZ",
+        "Thailand": "TH",
+        "Timor-Leste": "TL",
+        "Togo": "TG",
+        "Tokelau": "TK",
+        "Tonga": "TO",
+        "Trinidad and Tobago": "TT",
+        "Tunisia": "TN",
+        "Turkey": "TR",
+        "Turkmenistan": "TM",
+        "Turks and Caicos Islands": "TC",
+        "Tuvalu": "TV",
+        "Uganda": "UG",
+        "Ukraine": "UA",
+        "United Arab Emirates": "AE",
+        "United Kingdom": "GB",
+        "United States Minor Outlying Islands": "UM",
+        "United States of America": "US",
+        "Uruguay": "UY",
+        "Uzbekistan": "UZ",
+        "Vanuatu": "VU",
+        "Venezuela": "VE",
+        "Vietnam": "VN",
+        "Virgin Islands (British)": "VG",
+        "Virgin Islands (U.S.)": "VI",
+        "Wallis and Futuna": "WF",
+        "Western Sahara": "EH",
+        "Yemen": "YE",
+        "Zambia": "ZM",
+        "Zimbabwe": "ZW"
     }
 
     def rounded_area(self, geom):
@@ -59,17 +333,24 @@ class SCLStats(SCLTask):
             .multiply(0.1)
         )
 
+    def get_iso(self, feat):
+        name = feat.get(USDOS_NAME_FIELD)
+        iso = ee.Dictionary(self.ISO_COUNTRY_DICT).get(name, "")
+        return feat.set(ISO_FIELD, iso)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.error_margin = ee.ErrorMargin(1)
-        self.countries = ee.FeatureCollection(self.inputs["countries"]["ee_path"])
+        self.countries = ee.FeatureCollection(self.inputs["countries"]["ee_path"]).map(self.get_iso)\
+            .merge(self.inputs["leuser"]["ee_path"])
         self.ecoregions = ee.FeatureCollection(self.inputs["ecoregions"]["ee_path"])
         self.pas = ee.FeatureCollection(self.inputs["pas"]["ee_path"])
 
     def calc_landscapes(self, landscape_key):
-        landscapes, landscapes_date = self.get_most_recent_featurecollection(
-            self.inputs[landscape_key]["ee_path"]
-        )
+        # landscapes, landscapes_date = self.get_most_recent_featurecollection(
+        #     self.inputs[landscape_key]["ee_path"]
+        # )
+        landscapes = ee.FeatureCollection(self.inputs[landscape_key]["ee_path"])
 
         if landscapes is None:
             return
@@ -155,7 +436,7 @@ class SCLStats(SCLTask):
                 ).map(get_ls_countries_biome_numbers)
 
                 props = {
-                    "lscountry": country.get("iso_alpha2"),
+                    "lscountry": country.get(ISO_FIELD),
                     "ls_total_area": ls_total_area,
                     "lscountry_area": ls_country_area,
                     "areas": ls_country_biome_numbers,
@@ -183,9 +464,9 @@ class SCLStats(SCLTask):
 
     def calc(self):
         self.calc_landscapes(f"scl_{SCLTask.SPECIES}")
-        self.calc_landscapes(f"scl_{SCLTask.RESTORATION}")
+        # self.calc_landscapes(f"scl_{SCLTask.RESTORATION}")
         self.calc_landscapes(f"scl_{SCLTask.SURVEY}")
-        self.calc_landscapes(f"scl_{SCLTask.FRAGMENT}")
+        # self.calc_landscapes(f"scl_{SCLTask.FRAGMENT}")
 
     def check_inputs(self):
         super().check_inputs()
